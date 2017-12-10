9c6b5be2cd209451be7ecfec1b8ff9c2a991bd4b
Jhonnatthan <j-jhoe@hotmail.com>
2017-12-10 21:30:57 -0200

    Commit

--- app/Views/template.php
+++ app/Views/template.php
@@ -10,7 +10,7 @@
          
         <?php 
         	if (isset($viewName)) { 
-        		$path = viewsPath() . $viewName . '.php'; 
+        		$path = VIEW_PATH . $viewName . '.php'; 
         		if (file_exists($path)) { 
         			require_once $path; 
         		} 
